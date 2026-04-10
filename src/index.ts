c0057c76b87987dd885a6bed96c1911723b42f13
Sedin-HarishViswanath <harish.viswanath@sedintechnologies.com>
2026-04-10 11:46:25 +0530

    Index file added

---
+++ src/index.ts
@@ -0,0 +1,9 @@
+export { map } from './map';
+export { filter } from './filter';
+export { reduce } from './reduce';
+export { reduceRight } from './reduceRight';
+export { some } from './some';
+export { every } from './every';
+export { find } from './find';
+export { pluck } from './pluck';
+export { lens, view, set } from './lens';
